e4aafb4efc08a1539ad9193902214e231150ea27
HunnyJ434 <104228915+HunnyJ434@users.noreply.github.com>
2022-08-03 10:06:55 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        // use obj.fib(n) method to solve any Fibonacci sequence for given n.
         Fib obj1 = new Fib();
         System.out.println(obj1.fib(15));
     }
